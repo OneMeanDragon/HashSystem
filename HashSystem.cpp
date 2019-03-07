af504417fd299b199067bb92faa107249e32a002
OneMeanDragon <dragonkings@gmail.com>
2019-03-07 04:11:16 -0500

    Add files via upload

--- HashSystem.cpp
+++ HashSystem.cpp
@@ -6,55 +6,89 @@
 
 namespace ns_BNCS {
 	namespace HASH {
-		void data_hash(t_hash *result, const void *src, const int len, bool broken_sha) {
-			unsigned int a = 0x67452301u;
-			unsigned int b = 0xefcdab89u;
-			unsigned int c = 0x98badcfeu;
-			unsigned int d = 0x10325476u;
-			unsigned int e = 0xc3d2e1f0u;
-			unsigned int g;
-			int i;
-
-			unsigned char bBuffer[320];
+		void initial_values(t_hash *result)
+		{
+			(*result)[0] = 0x67452301u;
+			(*result)[1] = 0xefcdab89u;
+			(*result)[2] = 0x98badcfeu;
+			(*result)[3] = 0x10325476u;
+			(*result)[4] = 0xc3d2e1f0u;
+		}
+		void set_buffer(const void *input, const int len, UINT32 *buffer, bool broken_sha)
+		{
+			UCHAR tmpBuffer[320];
 			if (broken_sha) {
-				ZeroMemory(bBuffer, 320);
+				ZeroMemory(tmpBuffer, 320);
+				memcpy(tmpBuffer, input, len);
+				memcpy(buffer, tmpBuffer, 320);
+				return;
 			}
-			else {
-				ZeroMemory(bBuffer, (16 * 4));
-			}
+			ZeroMemory(tmpBuffer, (16 * 4));
+			memcpy(tmpBuffer, input, len);
 
-			memcpy(bBuffer, src, len);
+			UINT32 i;
+			UINT32 pos;
 
-			if (!broken_sha) {
-				bBuffer[len] = 0x80U;
-				*(UINT32*)(bBuffer + (15 * 4)) = (UINT32)(len * 8);
-			}
+			for (pos = 0, i = 0; i < 16; i++)
+			{
+				buffer[i] = 0;
 
-			UINT32 lpdwBuffer[80];
-			memcpy(lpdwBuffer, bBuffer, 320);
-			if (!broken_sha) {
-				for (int i = 0; i < 14; i++)
 				{
-					RENDIAN_DWORD(lpdwBuffer[i]);
+					if (pos < len)
+						buffer[i] |= ((UINT32)tmpBuffer[pos]) << 24;
+					else if (pos == len)
+						buffer[i] |= ((UINT32)0x80000000);
 				}
+				pos++;
+				{
+					if (pos < len)
+						buffer[i] |= ((UINT32)tmpBuffer[pos]) << 16;
+					else if (pos == len)
+						buffer[i] |= ((UINT32)0x800000);
+				}
+				pos++;
+				{
+					if (pos < len)
+						buffer[i] |= ((UINT32)tmpBuffer[pos]) << 8;
+					else if (pos == len)
+						buffer[i] |= ((UINT32)0x8000);
+				}
+				pos++;
+				{
+					if (pos < len)
+						buffer[i] |= ((UINT32)tmpBuffer[pos]);
+					else if (pos == len)
+						buffer[i] |= ((UINT32)0x80);
+				}
+				pos++;
 			}
+		}
+
+		void digest_hash(t_hash *result, UINT32 *buffer, bool broken_sha) {
+			UINT32 a = (*result)[0];
+			UINT32 b = (*result)[1];
+			UINT32 c = (*result)[2];
+			UINT32 d = (*result)[3];
+			UINT32 e = (*result)[4];
+			UINT32 g = 0;
+			int i;
 
 			for (i = 0; i < 80; ++i) {
 				if (i < 64)
 					if (broken_sha) {
-						lpdwBuffer[i + 16] = ROL(1, F4(lpdwBuffer[i], lpdwBuffer[i + 8], lpdwBuffer[i + 2], lpdwBuffer[i + 13]) % 32);
+						buffer[i + 16] = ROL(1, F4(buffer[i], buffer[i + 8], buffer[i + 2], buffer[i + 13]) % 32);
 					}
 					else {
-						lpdwBuffer[i + 16] = ROL(F4(lpdwBuffer[i], lpdwBuffer[i + 8], lpdwBuffer[i + 2], lpdwBuffer[i + 13]) % 32, 1);
+						buffer[i + 16] = ROL(F4(buffer[i], buffer[i + 8], buffer[i + 2], buffer[i + 13]) % 32, 1);
 					}
 				if (i < 20)
-					g = lpdwBuffer[i] + ROL(a, 5) + e + F1(b, c, d) + 0x5a827999lu;
+					g = buffer[i] + ROL(a, 5) + e + F1(b, c, d) + 0x5a827999lu;
 				else if (i < 40)
-					g = F2(d, c, b) + e + ROL(g, 5) + lpdwBuffer[i] + 0x6ed9eba1lu;
+					g = F2(d, c, b) + e + ROL(g, 5) + buffer[i] + 0x6ed9eba1lu;
 				else if (i < 60)
-					g = lpdwBuffer[i] + ROL(g, 5) + e + F3(c, b, d) - 0x70e44324lu;
+					g = buffer[i] + ROL(g, 5) + e + F3(c, b, d) - 0x70e44324lu;
 				else
-					g = F2(d, c, b) + e + ROL(g, 5) + lpdwBuffer[i] - 0x359d3e2alu;
+					g = F2(d, c, b) + e + ROL(g, 5) + buffer[i] - 0x359d3e2alu;
 				e = d;
 				d = c;
 				c = ROL(b, 30);
@@ -62,11 +96,60 @@ namespace ns_BNCS {
 				a = g;
 			}
 
-			(*result)[0] = 0x67452301u + g;
-			(*result)[1] = 0xefcdab89u + b;
-			(*result)[2] = 0x98badcfeu + c;
-			(*result)[3] = 0x10325476u + d;
-			(*result)[4] = 0xc3d2e1f0u + e;
+			(*result)[0] += g;
+			(*result)[1] += b;
+			(*result)[2] += c;
+			(*result)[3] += d;
+			(*result)[4] += e;
+		}
+
+		void data_hash(t_hash *result, const void *src, const int len, bool broken_sha)
+		{
+			initial_values(result);
+			UINT32 SHABuffer[80];
+			if (broken_sha) {
+				set_buffer(src, len, SHABuffer, true);
+				digest_hash(result, SHABuffer, true);
+			}
+			else {
+				UINT32 original_length = len;
+				UINT32 size = len;
+				UINT32 inc;
+				const UCHAR * data;
+				data = (const UCHAR *)(src);
+
+				while (size > 0) {
+					if (size >= 64) {
+						inc = 64;
+					}
+					else {
+						inc = size;
+					}
+
+					if (size >= 64)
+					{
+						set_buffer(data, inc, SHABuffer, false);
+						digest_hash(result, SHABuffer, false);
+					}
+					else if (size > 55) {
+
+						set_buffer(data, inc, SHABuffer, false);
+						digest_hash(result, SHABuffer, false);
+
+						// now use blizz variant as we only wanna fill in zeros
+						set_buffer(data, 0, SHABuffer, true); //set true gheyboi side
+						SHABuffer[15] |= (UINT32)(len * 8);
+						digest_hash(result, SHABuffer, false);
+					}
+					else {
+						set_buffer(data, inc, SHABuffer, false);
+						SHABuffer[15] |= (UINT32)(len * 8);
+						digest_hash(result, SHABuffer, false);
+					}
+					data += inc;
+					size -= inc;
+				}
+			}
 		}
 		bool matchhash(t_hash *a, t_hash *b) {
 			if (((*a)[0] == (*b)[0]) && ((*a)[1] == (*b)[1]) && ((*a)[2] == (*b)[2]) && ((*a)[3] == (*b)[3]) && ((*a)[4] == (*b)[4])) {
--- HashSystem.h
+++ HashSystem.h
@@ -2,6 +2,9 @@
 
 namespace ns_BNCS {
 	namespace HASH {
+		void initial_values(t_hash *result);
+		void set_buffer(const void *input, const int len, UINT32 *buffer, bool broken_sha);
+		void digest_hash(t_hash *result, UINT32 *buffer, bool broken_sha);
 		void data_hash(t_hash *result, const void *src, const int len, bool broken_sha);
 		bool matchhash(t_hash *a, t_hash *b);
 	}
